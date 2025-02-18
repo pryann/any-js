a7012e8fe55f5dc0277d04acb60450ddd395e3be
Gergely Gáll <pryann@gmail.com>
2025-02-18 16:44:40 +0100

    add lessons

---
+++ 02-day/js/numbers.js
@@ -0,0 +1,14 @@
+const num = 42
+const binaryNum = 0b101010
+console.log(binaryNum)
+const octalNum = 0o52
+console.log(octalNum)
+const hexNum = 0x2a
+console.log(hexNum)
+
+console.log(Math.PI)
+console.log(Math.random())
+// generate a random number between 1 and 6
+console.log(Math.floor(Math.random() * 6) + 1)
+console.log(Math.sqrt(16))
+console.log(Math.pow(2, 3)) // 2**3
